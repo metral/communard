55726c5db91e953d9ae04ae8d7cd88bda4d4dbc3
Mike Metral <mike.metral@rackspace.com>
2012-06-19 23:20:42 -0700

    Modified output messages

--- workstation.sh
+++ workstation.sh
@@ -19,34 +19,50 @@ log_file="install.log"
 
 ################################################################################
 
+## Welcome ##
+
+echo -e "\n** This script is intended for a Linux OpsCode Chef Workstation **"
+echo -e "\n** The packages that will be installed & setup: RVM, Ruby & Chef **"
+echo -e "Logs will be written to: $log_file"
+
+################################################################################
+
 ## Install dependencies ##
 
-sudo apt-get update
+echo -e "\n=> Updating apt-get repos..."
+sudo apt-get -qq update
+echo "==> done..."
 
+echo -e "\n=> Installing RVM dependencies from repos..."
 # TODO: pulled from `rvm requirements` - might want to script this parsing
-sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev \
+sudo apt-get install -qqq -y build-essential openssl libreadline6 libreadline6-dev \
 curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 \
 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake \
 libtool bison subversion libmysqlclient-dev nodejs
+echo "==> done..."
 
 ################################################################################
 
 ## Install RVM ##
 
-echo -e "\n=> Installing RVM...\n"
+echo -e "\n=> Installing RVM..."
 curl -O -L -k \
-    https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer
+    https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer \
+    > $log_file 2>&1
+	
 chmod +x rvm-installer
 
 "$PWD/rvm-installer" > $log_file 2>&1
 
-[[ -f rvm-installer ]] && rm -f rvm-installer
+[[ -f rvm-installer ]] > $log_file 2>&1 && rm -f rvm-installer
+
+echo "==> done..."
 
 ################################################################################
 
 ## Configure environment to work with RVM ##
 
-echo -e "\n=> Setting up RVM to load with new shells..."
+echo -e "=> Setting up RVM to load with new shells..."
 
 #if RVM is installed as user root it goes to /usr/local/rvm/ not ~/.rvm
 rvm_script_path="\"$HOME/.rvm/scripts/rvm\""
@@ -104,8 +120,8 @@ else
 ~/.rvm/bin/rvm install $ruby_version > $log_file 2>&1
 fi
 
-echo -e "\n==> done..."
-echo -e "\n=> Using $ruby_version and setting it as default for new shells..."
+echo -e "==> done..."
+echo -e "=> Using $ruby_version and setting it as default for new shells..."
 
 # Load RVM into shell
 [[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"
@@ -140,5 +156,11 @@ echo "==> done..."
 echo -e "\n=> Configuring user & organization keys + knife configuration..."
 # TODO: copy keys from dir
 echo "==> done..."
+
+################################################################################
+
+## Good-bye ##
+
+echo -e "\n** All done. Good-bye! **\n"
 
 ################################################################################
